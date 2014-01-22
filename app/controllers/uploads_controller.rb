dd85e1809fe4b3c7d8589fcd03e4381190dfe62c
Dustin Griffith <dgriffith@ubuntu.(none)>
2014-01-22 13:35:47 -0600

    Finished the webinar uploads page

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,6 +1,11 @@
 class UploadsController < ApplicationController
-  def index
+  def index 
     @uploads = Upload.all
+  end
+  def show 
+
+    @uploads = Upload.all
+    render :action => 'index'
   end
   def new
     @upload = Upload.new()
@@ -14,7 +19,17 @@ class UploadsController < ApplicationController
       flash[:notice] = "Could not create webinar"
     end
   end
+  def edit
+    @upload = Upload.find(params[:id])
+  end
   def update
+    @upload = Upload.find(params[:id])
+    if @upload.update_attributes(params[:upload])
+      flash[:notice] = "Successfully updates upload"
+      redirect_to upload_path
+    else
+      render :action => 'edit'
+    end
   end
   def destroy
     @upload = Upload.find(params[:id])
